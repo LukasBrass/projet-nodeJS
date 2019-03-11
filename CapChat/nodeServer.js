10930390a559cd3db444bc0c9c794230bdb242c6
Hastur <lukas.brasseleur@gmail.com>
2019-03-11 17:22:18 +0100

    Add files via upload

---
+++ CapChat/nodeServer.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var queryString = require('querystring');
+
+let handleRequest = (request, response) => {
+    var page = url.parse(request.url).pathname;
+    var params = queryString.parse(url.parse(request.url).query);
+    response.writeHead(200, {'Content-Type' : 'text/html;charset=utf-8'})
+    if(page == '/captcha') {
+        fs.readFile('./captcha.html', null, (error, data) => {
+            if(error) {
+                response.writeHead(400);
+                response.write('Error. Not captcha.html file found');
+            }
+            else {
+                response.write(data);
+            }
+            response.end();
+        });
+    } else {
+        response.writeHead(404);
+        response.write('File not found. This route is not defined yet.');
+        response.end();
+    }
+};
+
+http.createServer(handleRequest).listen(8080);
